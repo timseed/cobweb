1fee3d996a3075a40a6eda13219b26ae71ed2f0e
Tim Seed <tim@sy-edm.com>
2016-12-23 13:01:36 +0400

    Nearly there

--- cobweb/cobweb.py
+++ cobweb/cobweb.py
@@ -11,8 +11,8 @@ class cobweb(object):
                       (len/20,-Z)
                       ]
         self.side2 = [(-len / 2, 0),
-                      (-len / 2, -len),
-                      ((-len / 2) * 1 / 10, -len),
+                      (-len / 2, len),
+                      ((-len / 2) * 1 / 10, len),
                       ((-len / 2) * 1 / 10, len - Z),
                       (-len / 2, len - Z),
                       (-len / 2, 0 - Z),
@@ -22,9 +22,9 @@ class cobweb(object):
     def show(self):
         pre = (0, 0)
         for a in self.side1:
-            print('{:8} {:8} {:8}, {:8} {:8} {:8}'.format(pre[0], pre[1],0, a[0], a[1],0))
+            print('{:8},\t{:8},\t{:8},\t{:8},\t{:8},\t{:8},\t8.000e-04,\t-1'.format(pre[0], pre[1],0, a[0], a[1],0))
             pre = a
         pre = (0, 0)
         for a in self.side2:
-            print('{:8} {:8} {:8}, {:8} {:8} {:8}'.format(pre[0], pre[1],0, a[0], a[1],0))
+            print('{:8},\t{:8},\t{:8},\t{:8},\t{:8},\t{:8},\t8.000e-04,\t-1'.format(pre[0], pre[1],0, a[0], a[1],0))
             pre = a
